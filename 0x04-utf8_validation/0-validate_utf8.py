e8f7b1be651f28d4eb24737f624383fa052a2088
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-23 12:06:49 -0700

    Revert

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,8 +3,10 @@
 Module: UTF-8 validation.
 """
 
+
 def validUTF8(data):
-    """Checks if a list of integers are valid UTF-8 codepoints.
+    """
+    Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
     skip = 0
